5cdaf5ba265101d23317a101ad0072eac3929b36
udayvashishtha <vashishthatheuday@gmail.com>
2024-10-19 21:14:38 -0700

    Add program

---
+++ Array/Easy/Movezerotoend/Brute.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void Movezerotoend(int arr[],int n){
+
+    vector<int>temp;
+
+  
+    for(int i=0;i<n;i++){
+        
+        if(arr[i]!=0){
+            temp.push_back(arr[i]);
+        }
+    }
+
+    int nm=temp.size();
+
+    for(int i=0;i<nm;i++){
+        arr[i]=temp[i];
+    }
+
+    for(int j=nm;j<n;j++){
+        arr[j]=0;
+    }
+   
+    for(int j=0;j<n;j++){
+        cout<<arr[j]<<" ";
+    }
+}
+int main(){
+    int n;
+
+    cout<<"enter the size of array";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter the element in array";
+         cin>>arr[i];
+    }
+
+  
+    Movezerotoend(arr,n);
+    return 0 ;
+
+}
